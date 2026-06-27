63a74776e9a4f655a66080b4e8e0dea875ade392
Sharooz <sharooz7717@gmail.com>
2026-06-27 20:33:01 +0530

    Add Booking API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.MenuItemsView.as_view()),
+    path('<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuItemSerializer
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
 
-# Create your views here.
-def index(request):
-    return render(request, "index.html")
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(
+    generics.RetrieveUpdateAPIView,
+    generics.DestroyAPIView
+):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
